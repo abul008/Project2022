4ff5508153d4aebed1e07816bb7b9c18fa1ea1e1
albert azroyan <azroyan1997@mail.ru>
2022-12-17 11:27:02 -0800

    remove fetchTodos function

--- client/src/api/db/api_requests.tsx
+++ client/src/api/db/api_requests.tsx
@@ -1,7 +1,5 @@
-// import axios from 'axios'
 import { put } from './index'
 import {Book} from '../../types/index'
-
 
 export const postNewData = async (data: Book) => {
     put('book', data)
--- client/src/store/action-creators/book.ts
+++ client/src/store/action-creators/book.ts
@@ -1,6 +1,5 @@
 import {  Dispatch } from 'redux';
 import {BookAction, BookActionTypes ,Bookinfo as Booking} from "../types/book";
-import { getChannels } from "../../api/db/index";
 
 export function setBookPage(bookcase: Booking): BookAction {
     return {
@@ -8,27 +7,4 @@ export function setBookPage(bookcase: Booking): BookAction {
          payload: bookcase
         }
 }
-  
-
-export const fetchTodos = () => {
-        return async (dispatch: Dispatch<BookAction>) => {
-            try {
-                const response = await getChannels('api/v1/', 'get/book/')
-                setTimeout(() => {
-                    dispatch({type: BookActionTypes.BOOK_GET_INFO, payload: response.data})
-                }, 500)
-            } catch (e) {
-                dispatch({
-                    type: BookActionTypes.BOOK_GET_INFO,
-                    _payload: 'Произошла ошибка при загрузке списка дел',
-                    get payload() {
-                        return this._payload;
-                    },
-                    set payload(value) {
-                        this._payload = value;
-                    },
-                })
-            }
-        }
-    }
 
